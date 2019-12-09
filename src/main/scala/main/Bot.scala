1f84997e0fcd144aa88c02649707563c398249ba
evgenii <ibanez.n04@gmail.com>
2019-12-09 18:50:13 +0300

    added tgApi

---
+++ src/main/scala/main/Bot.scala
@@ -0,0 +1,30 @@
+package main
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.{Message, Update}
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+
+class Bot extends TelegramLongPollingBot {
+
+  override def onUpdateReceived(update: Update): Unit = {
+    println(
+      update.getMessage.getText + " by " + update.getMessage.getFrom.getFirstName
+    )
+
+    val message = new SendMessage(update.getMessage.getChatId, "hello, friend ")
+
+    try execute[Message, SendMessage](message)
+    catch {
+      case e: TelegramApiException =>
+        e.printStackTrace()
+    }
+
+  }
+
+  override def getBotUsername: String = "GreatNewsBot"
+
+  override def getBotToken: String =
+    "823708273:AAEsJrfv8U8kgw3zrM8izOCal_ybaMjGfNw"
+
+}
---
+++ src/main/scala/main/Main.scala
@@ -0,0 +1,32 @@
+package main
+
+import com.typesafe.scalalogging.Logger
+import org.drinkless.tdlib.example.Example
+import org.telegram.telegrambots.ApiContextInitializer
+import org.telegram.telegrambots.meta.TelegramBotsApi
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException
+
+object Main extends App {
+
+  //to call tdlib Api
+  // -Djava.library.path=<absolute path to these sources>/src/main/java/org/drinkless/tdlib/example
+  // needs to be added to VM options
+
+  //class Example - is the example of usage
+  Example.main(arguments)
+  val bot = new Bot()
+
+  val logger = Logger("Main")
+  ApiContextInitializer.init()
+  val botsApi = new TelegramBotsApi()
+  val arguments = Array[String]()
+
+  try botsApi.registerBot(bot)
+  catch {
+    case ex: TelegramApiRequestException =>
+      logger.error(s"Error $ex")
+      ex.printStackTrace()
+  }
+  logger.info("Bot Started")
+
+}
