33f9b148be60a0288eeada12fba1a3fb7a043a02
evgenii <ibanez.n04@gmail.com>
2019-12-09 20:29:18 +0300

    compiled code

--- src/main/scala/main/tdapi/TdApi.scala
+++ src/main/scala/main/tdapi/TdApi.scala
@@ -1,16 +1,29 @@
 package main.tdapi
 
+import java.io.{BufferedReader, IOException, InputStreamReader}
+import java.util.concurrent.locks.{Condition, Lock, ReentrantLock}
+
 import com.typesafe.scalalogging.Logger
 import org.drinkless.tdlib.Client.{ExceptionHandler, ResultHandler}
 import org.drinkless.tdlib.example.Example
 import org.drinkless.tdlib.{Client, TdApi}
 
 class TdApi {
+  val authorizationLock: Lock = new ReentrantLock
   val logger: Logger = Logger("TdApi")
+  val gotAuthorization: Condition = authorizationLock.newCondition
+  val newLine: String = System.getProperty("line.separator")
+  val commandsLine: String =
+    "Enter command (gcs - GetChats, gc <chatId> - GetChat, me - GetMe, sm <chatId> <message> - SendMessage, lo - LogOut, q - Quit): "
+  var client: Client = _
+  var authorizationState: TdApi.AuthorizationState = _
+  var haveAuthorization: Boolean = false
+  var quiting: Boolean = false
+  var currentPrompt: String = _
 
   def onAuthorizationStateUpdated(
-                                   authorizationState: TdApi.AuthorizationState
-                                 ): Unit = {
+    authorizationState: TdApi.AuthorizationState
+  ): Unit = {
     if (authorizationState != null) {
       Example.authorizationState = authorizationState
     }
@@ -21,7 +34,7 @@ class TdApi {
         parameters.useMessageDatabase = true
         parameters.useSecretChats = false
         parameters.apiId = 1157399
-        parameters.apiHash = "a3406de8d171bb422bb6ddf3bbd800e2"
+        parameters.apiHash = "50563ed845128e3e34c2d01f3a9ffc86"
         parameters.systemLanguageCode = "en"
         parameters.deviceModel = "Desktop"
         parameters.systemVersion = "Unknown"
@@ -84,7 +97,7 @@ class TdApi {
       case TdApi.AuthorizationStateClosed.CONSTRUCTOR =>
         print("Closed")
         if (!quiting)
-          client = Client.create(new Example.UpdatesHandler, null, null) // recreate client after previous has closed
+          client = Client.create(resultHandler, null, defHandler) // recreate client after previous has closed
 
       case _ =>
         System.err.println(
@@ -98,79 +111,17 @@ class TdApi {
   val defHandler: ExceptionHandler = (res: TdApi.Object) =>
     logger.info(res.toString)
 
-  val tdApi: Client = Client.create(resultHandler, null, defHandler)
-  def joinChat(): Unit = {}
-}
-
-public static void onAuthorizationStateUpdated(TdApi.AuthorizationState authorizationState) {
-  if (authorizationState != null) {
-    Example.authorizationState = authorizationState;
-  }
-  switch (Example.authorizationState.getConstructor()) {
-    case TdApi.AuthorizationStateWaitTdlibParameters.CONSTRUCTOR:
-    TdApi.TdlibParameters parameters = new TdApi.TdlibParameters();
-    parameters.databaseDirectory = "tdlib";
-    parameters.useMessageDatabase = true;
-    parameters.useSecretChats = true;
-    parameters.apiId = 94575;
-    parameters.apiHash = "a3406de8d171bb422bb6ddf3bbd800e2";
-    parameters.systemLanguageCode = "en";
-    parameters.deviceModel = "Desktop";
-    parameters.systemVersion = "Unknown";
-    parameters.applicationVersion = "1.0";
-    parameters.enableStorageOptimizer = true;
-
-    client.send(new TdApi.SetTdlibParameters(parameters), new AuthorizationRequestHandler());
-    break;
-    case TdApi.AuthorizationStateWaitEncryptionKey.CONSTRUCTOR:
-      client.send(new TdApi.CheckDatabaseEncryptionKey(), new AuthorizationRequestHandler());
-    break;
-    case TdApi.AuthorizationStateWaitPhoneNumber.CONSTRUCTOR: {
-      String phoneNumber = promptString("Please enter phone number: ");
-      client.send(new TdApi.SetAuthenticationPhoneNumber(phoneNumber, null), new AuthorizationRequestHandler());
-      break;
+  def promptString(prompt: String): String = {
+    print(prompt)
+    currentPrompt = prompt
+    val reader = new BufferedReader(new InputStreamReader(System.in))
+    var str = ""
+    try str = reader.readLine
+    catch {
+      case e: IOException =>
+        e.printStackTrace()
     }
-    case TdApi.AuthorizationStateWaitCode.CONSTRUCTOR: {
-      String code = promptString("Please enter authentication code: ");
-      client.send(new TdApi.CheckAuthenticationCode(code), new AuthorizationRequestHandler());
-      break;
-    }
-    case TdApi.AuthorizationStateWaitRegistration.CONSTRUCTOR: {
-      String firstName = promptString("Please enter your first name: ");
-      String lastName = promptString("Please enter your last name: ");
-      client.send(new TdApi.RegisterUser(firstName, lastName), new AuthorizationRequestHandler());
-      break;
-    }
-    case TdApi.AuthorizationStateWaitPassword.CONSTRUCTOR: {
-      String password = promptString("Please enter password: ");
-      client.send(new TdApi.CheckAuthenticationPassword(password), new AuthorizationRequestHandler());
-      break;
-    }
-    case TdApi.AuthorizationStateReady.CONSTRUCTOR:
-      haveAuthorization = true;
-    authorizationLock.lock();
-    try {
-      gotAuthorization.signal();
-    } finally {
-      authorizationLock.unlock();
-    }
-    break;
-    case TdApi.AuthorizationStateLoggingOut.CONSTRUCTOR:
-      haveAuthorization = false;
-    print("Logging out");
-    break;
-    case TdApi.AuthorizationStateClosing.CONSTRUCTOR:
-      haveAuthorization = false;
-    print("Closing");
-    break;
-    case TdApi.AuthorizationStateClosed.CONSTRUCTOR:
-      print("Closed");
-    if (!quiting) {
-      client = Client.create(new UpdatesHandler(), null, null); // recreate client after previous has closed
-    }
-    break;
-    default:
-      System.err.println("Unsupported authorization state:" + newLine + Example.authorizationState);
+    currentPrompt = null
+    str
   }
 }
-
